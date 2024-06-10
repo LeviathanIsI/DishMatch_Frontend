ee70ebab932eebb9985aae8a1c6d3d671e0ce6f2
LeviathanIsI <64034102+LeviathanIsI@users.noreply.github.com>
2024-06-10 16:48:41 -0400

    Added backend url

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://dishmatch-635d886301c4.herokuapp.com/";
 
 const apiFetch = async (endpoint, options = {}) => {
   const token = localStorage.getItem("token");
